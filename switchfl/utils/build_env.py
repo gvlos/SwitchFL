30c079d8b2a7965ccabc7b8dab07452ed6a68c21
Robin Uhrich <robin.uhrich@gmail.com>
2025-07-30 13:12:37 +0200

    added build function to build standard asycn switch env

---
+++ switchfl/utils/build_env.py
@@ -0,0 +1,33 @@
+from flatland.envs.rail_env import RailEnv
+from flatland.envs.rail_generators import sparse_rail_generator
+from flatland.envs.line_generators import sparse_line_generator
+from switchfl.switch_env import ASyncSwitchEnv
+
+
+def build_standard_async_env(
+    height: int,
+    width: int,
+    max_num_cities: int,
+    num_trains: int,
+    max_rails_between_cities: int = 1,
+    max_rail_pairs_in_city: int = 1,
+    grid_mode: bool = True,
+    render_mode: str = None,
+    seed: int = None,
+):
+    rail_env = RailEnv(
+        width=width,
+        height=height,
+        rail_generator=sparse_rail_generator(
+            max_num_cities=max_num_cities,
+            grid_mode=grid_mode,
+            max_rails_between_cities=max_rails_between_cities,
+            max_rail_pairs_in_city=max_rail_pairs_in_city,
+            seed=seed,
+        ),
+        line_generator=sparse_line_generator(seed=seed),
+        number_of_agents=num_trains,
+    )
+
+    env = ASyncSwitchEnv(rail_env, render_mode=render_mode)
+    return env
