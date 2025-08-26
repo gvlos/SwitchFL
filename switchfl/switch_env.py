33976470465ec34961144aa5d7340da035f0df11
Robin Uhrich <robin.uhrich@gmail.com>
2025-08-26 17:57:39 +0200

    implemented monitoring system for trains.

--- switchfl/switch_env.py
+++ switchfl/switch_env.py
@@ -17,8 +17,8 @@ from pettingzoo import AECEnv
 from switchfl import NodeId, TrainAgentHandle
 from switchfl.observer import StandardObserver, _Observer
 from switchfl.rail_network import RailNetwork
-from switchfl.utils.naming import (name2switch_id, switch_id2name,
-                                   symmetric_string)
+from switchfl.utils.logging import format_logger
+from switchfl.utils.naming import name2switch_id, switch_id2name, symmetric_string
 
 
 class _SwitchEnv:
@@ -34,7 +34,8 @@ class _SwitchEnv:
     ):
         super().__init__()
         self.logger = logging.getLogger(type(self).__name__)
-        
+        self.logger = format_logger(self.logger)
+
         self.rail_env = rail_env
         self.render_mode = render_mode
         self.max_steps = max_steps
@@ -189,22 +190,25 @@ class _SwitchEnv:
             # will send the train forward which messes up the scheduling and planning
             next_train_actions = train_actions[train_agent_handle]
             if (
-                moving_train is not None 
+                moving_train is not None
                 and self.rail_network.get_port_distance(out_port, next_port) == 0
-                and len(self.train_action_plan[train_agent_handle]) > 0  
+                and len(self.train_action_plan[train_agent_handle]) > 0
             ):
-                # cut away the MOVE_FORWARD action of the new plan, because 
+                # cut away the MOVE_FORWARD action of the new plan, because
                 # moving into the new switch is done by the action from the previous switch
                 next_train_actions.pop(0)
                 self.train_action_plan[train_agent_handle].extend(next_train_actions)
-            
+
             # moving train is None -> stop moving command (stop the train before it enters the next switch)
-            # but retain information about how the train will enter the next switch 
-            # example: move left leads directly on another switch 
-            elif moving_train is None and len(self.train_action_plan[train_agent_handle]) > 0:
+            # but retain information about how the train will enter the next switch
+            # example: move left leads directly on another switch
+            elif (
+                moving_train is None
+                and len(self.train_action_plan[train_agent_handle]) > 0
+            ):
                 stop_action = next_train_actions[0]
                 assert stop_action == RailEnvActions.STOP_MOVING
-                self.train_action_plan[train_agent_handle].insert(0, stop_action    )
+                self.train_action_plan[train_agent_handle].insert(0, stop_action)
             else:
                 self.train_action_plan[train_agent_handle].extend(next_train_actions)
 
@@ -216,20 +220,19 @@ class _SwitchEnv:
         Also take into account if there is a next action, pre-computed for a train to transit the switch
         """
         # NOTE: the time when the train departures is already taken into account
-        action = {}
+        train_actions = {}
         for train in self.rail_env.agents:
             handle = train.handle
             if self.train_done[handle]:
-                # no act
-                # ion for already done trains
+                # no action for already done trains
                 continue
 
             if len(self.train_action_plan[handle]) == 0:
                 # use base action
-                action[handle] = RailEnvActions.MOVE_FORWARD
+                train_actions[handle] = RailEnvActions.MOVE_FORWARD
             else:
                 # use predetermined action
-                action[handle] = self.train_action_plan[handle].pop(0)
+                train_actions[handle] = self.train_action_plan[handle].pop(0)
 
         # do rail env step
         (
@@ -237,8 +240,9 @@ class _SwitchEnv:
             self.train_reward,
             self.train_done,
             self.train_info,
-        ) = self.rail_env.step(action)
+        ) = self.rail_env.step(train_actions)
         self.rail_env_time += 1
+        self._check_action_execution()
 
         # check for all trains being done
         if self.train_done["__all__"]:
@@ -252,7 +256,7 @@ class _SwitchEnv:
         while len(self.active_switch_agents) == 0 and not self.terminated:
             # NOTE: after resetting the environment all trains are in a standstill
             # -> they have to be moved first after the reset
-            self._move_trains() 
+            self._move_trains()
             self._check_active_switch()
 
         # remove duplicates in agents but maintaining order
@@ -270,17 +274,6 @@ class _SwitchEnv:
         """do simulation step and see if a train enters a switch node
         -> then add the switch to active agents
         """
-
-        # # check if a train is ready to depart -> its position on the grid is known
-        # current_positions = [
-        #     train.position
-        #     for train in self.rail_env.agents
-        #     if train.position is not None
-        # ]
-        # if len(current_positions) == 0:
-        #     # if the train is not on the grid
-        #     return
-
         for train in self.rail_env.agents:
             # train is not one the grid yet or if it waiting don't execute something on it.
             if train.position is None or train.state == TrainState.WAITING:
@@ -305,10 +298,11 @@ class _SwitchEnv:
                 train.direction,
             )
 
-            
             # if with the next action the train has entered a switch add the switch to the active switches
             next_switch = self.rail_network.get_switch_on_position(new_position)
-            self.logger.debug(f"Next switch for train {train.handle} ({train.state.name, train.position}): {next_switch}")
+            self.logger.debug(
+                f"Next switch for train {train.handle} ({train.state.name, train.position}): {next_switch}"
+            )
             if next_switch is not None and (
                 train.state == TrainState.READY_TO_DEPART
                 or train.state == TrainState.MOVING
@@ -316,6 +310,25 @@ class _SwitchEnv:
                 # use new pos because the switch coordinates are its node_id
                 switch_id = switch_id2name(new_position)
                 self.active_switch_agents.append(switch_id)
+
+    def _check_action_execution(self):
+        """checks if the action has been executed successfully
+
+        This function will log an error if a train deviates from its planned path.
+        """
+        for train in self.rail_env.agents:
+            next_port = self.rail_network.get_trains_next_port(train)
+            prev_port = self.rail_network.get_trains_prev_port(train)
+            if next_port is None or prev_port is None:
+                # train is not at a switch or has no next port planned
+                continue
+            source_node = self.rail_network.get_switch_on_port(prev_port).id
+            target_node = self.rail_network.get_switch_on_port(next_port).id
+            rail_pieces = self.rail_network.get_rail_pieces_between_ports(
+                source_node, target_node
+            )
+            if train.position not in [*rail_pieces, source_node, target_node]:
+                self.logger.error(f"Train {train.handle} deviated from planned path!")
 
     def _init_semaphores(self):
         """executed in reset()
@@ -338,7 +351,7 @@ class _SwitchEnv:
                 current_direction = train.initial_direction
 
             while True:
-                if (
+                if (            
                     self.rail_network.get_switch_on_position(current_position)
                     is not None
                 ):
@@ -373,10 +386,10 @@ class _SwitchEnv:
             int: _description_
         """
         return sum(self.step_counter.values())
-    
+
     def get_env_plan(self, path: Path = None):
-        """Extract information about each switch as a csv from the environment: 
-        This csv should contain: 
+        """Extract information about each switch as a csv from the environment:
+        This csv should contain:
             - switch_id
             - action (int)
             - rail env actions (move forward, move_right, move_left, stop) with their corresponding ports
@@ -385,10 +398,11 @@ class _SwitchEnv:
         env_plan = []
         for _, switch in self.rail_network.switches:
             for action_idx in range(switch.n_actions):
-                env_plan.append(self.rail_network.get_switch_transition_info(
-                    switch.id,
-                    action=action_idx
-                ))
+                env_plan.append(
+                    self.rail_network.get_switch_transition_info(
+                        switch.id, action=action_idx
+                    )
+                )
 
         if path is not None:
             df = pd.DataFrame(env_plan)
@@ -401,9 +415,15 @@ class _SwitchEnv:
 
 class ASyncSwitchEnv(_SwitchEnv, AECEnv):
     def __init__(
-        self, rail_env, max_steps=200, render_mode=None, observer=None, seed=None
+        self,
+        rail_env,
+        max_steps=200,
+        render_mode=None,
+        observer=None,
+        monitoring=False,
+        seed=None,
     ):
-        super().__init__(rail_env, max_steps, render_mode, observer, seed)
+        super().__init__(rail_env, max_steps, render_mode, observer, monitoring, seed)
 
     def agent_iter(self, max_iter=2**63):
         while not (self.terminated or self.truncated):
@@ -425,7 +445,7 @@ class ASyncSwitchEnv(_SwitchEnv, AECEnv):
         # no switches with non processed trains left -> time to move trains
         if len(self.active_switch_agents) == 0:
             self._move_trains_to_switch()
-            
+
         # check for done episode
         self.step_counter[self.agent_selection] += 1
         if self.n_steps > self.max_steps:
@@ -449,5 +469,3 @@ class ASyncSwitchEnv(_SwitchEnv, AECEnv):
 
     def close(self):
         return super().close()
-    
-    
